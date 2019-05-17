1a45fcdadea92515d0f9dc00f646f0a0b58be539
Lolkot <49728137+Lolkot@users.noreply.github.com>
2019-05-17 20:57:35 +0700

    Update pract.h

--- pract.h
+++ pract.h
@@ -12,7 +12,7 @@ typedef struct rational_fraction {
     void sub (fraction dr1, fraction dr2, fraction *pdr);
     fraction *mult (fraction dr1, fraction dr2);
     fraction divide (fraction *pd1, fraction *pd2);
-    int c(char *p);
+    
 
 
 #endif // PRACT_H_INCLUDED
